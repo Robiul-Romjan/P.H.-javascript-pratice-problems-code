b6e7d33af1c0382079b8f0bbafeac90e88f02986
robiul-romjan <robiulromjan7@gmail.com>
2023-02-04 15:28:24 +0600

    some problem solve

--- if-else-for-loop.js
+++ if-else-for-loop.js
@@ -1,3 +1,30 @@
-if(true){
-    console.log('ok');
+// 1 theke 50 jugfol ber korar code ----------------------------------------------------------------------------->
+// function sumAllNumber(){
+//     let sumNumber = 0;
+//     for(let i = 1; i<=50; i++){
+//        sumNumber += i;
+//     }
+//     console.log(sumNumber);
+// }
+// sumAllNumber();
+
+// 1 theke 50 even number ber korar code -------------------------------------------------------------------------->
+// function getEvenNumber(){
+//     for(let i = 0; i<=50; i++){
+//      if(i % 2 === 0){
+//         console.log(i);
+//      }
+//     }
+// }
+// getEvenNumber();
+
+
+1 theke 50 odd number ber korar code -------------------------------------------------------------------------->
+function getOddNumber(){
+    for(let i = 0; i<=50; i++){
+     if(i % 2 !== 0){
+        console.log(i);
+     }
+    }
 }
+getOddNumber();
--- variabls-string-array.js
+++ variabls-string-array.js
@@ -1,5 +1,33 @@
-let a = 10;  
-let b = 20;
+ 3ta number er gunfol ber kore seti return kora---------------------------------------------------------------->
+function mulOfThreeNumbers(num1, num2, num3){
+    let result = num1 * num2 * num3;
+    return result
+}
+let answer = mulOfThreeNumbers(3, 5, 2);
+console.log(answer);
 
-let sum = a + b;
-console.log(sum);
+Array---------------------------------------------------------------------------------------------------------->
+let nameOfAllah = ['Allahu', 'Rahmanu', 'Rahimu', 'Maliku', 'Kuddusu', 'Salamu', 'Mominu', 'Muhaiminu', 'Jobbaru'];
+koita nam ache cheack--------
+console.log(nameOfAllah.length);
+4 number namta change kore onno nam bosanu------
+console.log(nameOfAllah[3] = "Ajiju");
+array tar modde 2 ta element jug kora-------
+nameOfAllah.push("Kabir", "Hakim");
+array tar modde 2 ta element ber kora kora-------
+nameOfAllah.pop();
+console.log(nameOfAllah);
+
+for loop using arry to show all elements----------------------------------------------------------------------->
+let nameOfAllah = ['Allahu', 'Rahmanu', 'Rahimu', 'Maliku', 'Kuddusu', 'Salamu', 'Mominu', 'Muhaiminu', 'Jobbaru'];
+for(let i = 0; i < nameOfAllah.length; i++){
+   console.log(nameOfAllah[i]);
+}
+
+kuno arry te 80 er theke beshi sonkha thakle segulu dekhau---------------------------------------------------->
+let numbers = [1,33,66,88,99,44,55,77,98,80,444,666,777,111];
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] > 80){
+        console.log(numbers[i]);
+    }
+}
